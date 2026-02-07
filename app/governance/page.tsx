21248fbee1df869a4bf7f9e9dab9c909906fa6b6
Tyche1107 <broosbook@gmail.com>
2026-02-06 23:14:12 -0800

    fix: governance page SSR crash - dynamic import Web3 components
    WagmiProvider not available during prerender, use dynamic import with ssr:false

--- app/governance/page.tsx
+++ app/governance/page.tsx
@@ -1,5 +1,7 @@
-import { ProposalList } from '@/app/components/governance/ProposalList'
-import { GovernanceHeader } from '@/app/components/governance/GovernanceHeader'
+import dynamic from 'next/dynamic'
+
+const ProposalList = dynamic(() => import('@/app/components/governance/ProposalList').then(m => m.ProposalList), { ssr: false })
+const GovernanceHeader = dynamic(() => import('@/app/components/governance/GovernanceHeader').then(m => m.GovernanceHeader), { ssr: false })
 
 export const metadata = {
   title: 'Governance',
