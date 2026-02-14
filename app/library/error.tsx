a003d21b70bbdcb6be87fbfcc0c273f6059a89e0
Tyche1107 <broosbook@gmail.com>
2026-02-13 17:43:50 -0800

    add missing error.tsx for library, multi-chain, logout routes

--- app/library/error.tsx
+++ app/library/error.tsx
@@ -3,5 +3,5 @@
 import RouteError from '@/app/components/RouteError'
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
-  return <RouteError error={error} reset={reset} contextLabel="LibraryDetail" />
+  return <RouteError error={error} reset={reset} contextLabel="Library" />
 }
