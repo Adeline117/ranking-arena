cc41e0618aeba7583f352138f23913c50f41441e
Tyche1107 <broosbook@gmail.com>
2026-03-05 17:38:35 -0800

    auto: lib/cron/fetchers/bitget-spot.ts

--- lib/cron/fetchers/uniswap.ts
+++ lib/cron/fetchers/uniswap.ts
@@ -61,7 +61,8 @@ async function aggregateTraders(): Promise<
         prev.count += 1
         addrMap.set(addr, prev)
       }
-    } catch {
+    } catch (err) {
+      logger.warn(`[${SOURCE}] Swap aggregation page failed: ${err instanceof Error ? err.message : String(err)}`)
       break
     }
   }
